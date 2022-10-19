162a4cd5e7d90c3fe84da9db5c478179f71c78ff
nyawall <107533190+nyawall@users.noreply.github.com>
2022-10-19 18:03:23 +0900

    lesson3

--- My project/Assets/scripts/move.cs
+++ My project/Assets/scripts/move.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-
+    public float MoveSpeed = 3.0f;
+    public float RotateSpeed = 360.0f;
     float h;
     float v;
+    float _r;
     private void Update()
     {
+        // Input.GetKey(KeyCode.UpArrow);
         h = Input.GetAxisRaw("Horizontal");
         Debug.Log(h);
         v = Input.GetAxisRaw("Vertical");
+        _r = Input.GetAxis("Mouse X");
     }
     private void FixedUpdate()
     {
         Vector3 dir = new Vector3(h, 0.0f, v).normalized;
-        transform.position += dir * Time.fixedDeltaTime;
+
+        // 이동벡터 = 속도 * 시간
+        // 이동벡터 변화량 = 속도 * 시간 변화량 
+        //transform.position += dir * Time.fixedDeltaTime;
+        transform.Translate(dir * MoveSpeed * Time.fixedDeltaTime, Space.World);
         //transform.position += Vector3.forward * v * Time.fixedDeltaTime;
+        //Quaternion
+        // 사원수, 각도를 4개의 원소로 표현하기 위한 체계
+        // eular 각도를 사용하면 짐벌락 문제가 생김.
+        // 짐벌락 : 두개의 축이 겹치면서 (조인트) 자유도를 상실하는 현상 [양자형고리]
+        // 연산이 ealar 각도 체계보다 가볍다
+        transform.Rotate(Vector3.up * _r * RotateSpeed * Time.fixedDeltaTime);
+
     }
 }
 
--- My project/Assets/scripts/test.cs
+++ My project/Assets/scripts/test.cs
@@ -15,52 +15,87 @@ public class test : MonoBehaviour
     {
         Debug.Log("[test] : OnEnable");
     }
+
+    
     private void Reset()
     {
         Debug.Log("[TEST] : Reset");
     }
+    /// <summary>
+    /// update 함수가 호출되기 전에 한번 실행되는 함수
+    /// </summary>
     private void Start()
     {
         Debug.Log("[test] : Start");
     }
-
+    /// <summary>
+    /// 게임 로직 매 프레임 처음 마다 호출되는 함수
+    /// </summary>
     private void Update()
     {
         Debug.Log("[test] : Update");
     }
-
+    /// <summary>
+    /// 게임 로직 매 프레임 마지막 마다 호출되는 함수
+    /// </summary>
     private void LateUpdate()
     {
         Debug.Log("[Test] : LateUpdate");
     }
+    /// <summary>
+    /// 물리 연산을 위한 프레임 마다 호출되는 함수
+    /// </summary>
     private void FixedUpdate()
     {
         Debug.Log("[Test] : FixedUpdate");
     }
+
+    /// <summary>
+    ///  editor 에서  gizmo(에디터상의 그래픽적인 부가 요소들)들을 그릴 때 호출되는 함수
+    /// </summary>
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireCube(transform.position, Vector3.one);
     }
+
+    /// <summary>
+    /// UI이벤트를 처리하기 위한 함수
+    /// </summary>
     private void OnGUI()
     {
         Event e = Event.current;
         Debug.Log(e.mousePosition);
     }
+    /// <summary>
+    /// 어플리케이션이 일시정지 될때 호출
+    /// </summary>
+    /// <param name="pause"></param>
     private void OnApplicationPause(bool pause)
     {
         Debug.Log($"[Test] : paused = {pause}");
     }
 
+
+    /// <summary>
+    ///  어플리케이션이 종료될때 호출
+    /// </summary>
     private void OnApplicationQuit()
     {
         Debug.Log("[Test] : applicationQuit");
     }
 
+    /// <summary>
+    /// Script Instance 가 비활성화될 때 마다 호출
+    /// </summary>
     private void OnDisable()
     {
         Debug.Log("[test] : disable");
     }
+
+    /// <summary>
+    /// 파괴될 때 호출
+    /// </summary>
     private void OnDestroy()
     {
         Debug.Log("[test] : Destroyed");
